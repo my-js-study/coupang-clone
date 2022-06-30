264ea8546cb0935ffdc79a4352c7a589937ead11
parksil0 <parksil00@naver.com>
2022-06-30 18:33:56 +0900

    chore(type): api request type 설정

---
+++ src/types/api.ts
@@ -0,0 +1,23 @@
+type Ad = {
+  email: boolean;
+  sms: boolean;
+  app: boolean;
+};
+
+type SignupAgreements = {
+  privacy: boolean;
+  ad: Ad | false;
+};
+
+export interface SignupRequest {
+  email: string;
+  password: string;
+  name: string;
+  phoneNumber: string;
+  agreements: SignupAgreements;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
