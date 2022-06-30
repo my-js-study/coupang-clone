b35b6b20db1416017674cfca7a8907e43d4b934d
parksil0 <parksil00@naver.com>
2022-06-30 18:33:20 +0900

    feat(axios): axios interceptor response 설정

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,18 +1,7 @@
 import cookies from "js-cookie";
-import { COOKIE_EXPIRES, COOKIE_KEYS } from "../constants/cookie";
+import { COOKIE_KEYS } from "../constants/cookie";
+import type { LoginRequest, SignupRequest } from "../types/api";
 import { api } from "../utils/api";
-import { setAuthCookies } from "../utils/cookie";
-
-type SignupAgreements = {
-  privacy: boolean;
-  ad:
-    | {
-        email: boolean;
-        sms: boolean;
-        app: boolean;
-      }
-    | false;
-};
 
 class AuthService {
   /** refreshToken을 이용해 새로운 토큰을 발급받습니다. */
@@ -22,39 +11,33 @@ class AuthService {
       return;
     }
 
-    const { data } = await api.post("/auth/refresh", null, {
+    await api.post("/auth/refresh", null, {
       headers: {
         Authorization: `Bearer ${refreshToken}`,
       },
     });
-
-    setAuthCookies(data.access, data.refresh);
   }
 
   /** 새로운 계정을 생성하고 토큰을 발급받습니다. */
-  async signup(
-    email: string,
-    password: string,
-    name: string,
-    phoneNumber: string,
-    agreements: SignupAgreements
-  ) {
-    const { data } = await api.post("/auth/signup", {
+  async signup({
+    email,
+    password,
+    name,
+    phoneNumber,
+    agreements,
+  }: SignupRequest) {
+    await api.post("/auth/signup", {
       email,
       password,
       name,
       phoneNumber,
       agreements,
     });
-
-    setAuthCookies(data.access, data.refresh);
   }
 
   /** 이미 생성된 계정의 토큰을 발급받습니다. */
-  async login(email: string, password: string) {
-    const { data } = await api.post("/auth/login", { email, password });
-
-    setAuthCookies(data.access, data.refresh);
+  async login({ email, password }: LoginRequest) {
+    await api.post("/auth/login", { email, password });
   }
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
+import cookies from "js-cookie";
+import { COOKIE_EXPIRES, COOKIE_KEYS } from "../constants/cookie";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_HOST,
+});
+
+api.interceptors.response.use((response) => {
+  if (response.data.access) {
+    cookies.set(COOKIE_KEYS.ACCESS_TOKEN, response.data.access, {
+      expires: COOKIE_EXPIRES.A_DAY,
+    });
+  }
+
+  if (response.data.refresh) {
+    cookies.set(COOKIE_KEYS.REFRESH_TOKEN, response.data.refresh, {
+      expires: COOKIE_EXPIRES.A_WEEK,
+    });
+  }
+
+  return response;
 });
