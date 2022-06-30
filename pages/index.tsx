1644eec112a58fc58e6d0d3206e374232e576462
parksil0 <parksil00@naver.com>
2022-06-30 18:50:04 +0900

    refactor(component): hook -> custom hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useQuery } from "react-query";
 
-import { UserService } from "../src/services";
-
+import useMe from "../src/hooks/query/useMe";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const { data: me } = useQuery("me", UserService.me, {
+  const me = useMe({
     refetchInterval: 500,
+    onSuccess: (data) => {
+      console.log("내 정보입니다", data);
+    },
   });
-
-  console.log("내 정보입니다", me);
 
   return (
     <div className={styles.container}>
